bf25161065534c49b84247466881837178ede4fd
{vongocchinh} <{ngocchinh1410@gmail.com}>
2021-07-18 20:00:47 +0700

    update

--- src/myApp/Home/container/Home.js
+++ src/myApp/Home/container/Home.js
@@ -1,8 +1,39 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
 import HomeComponent from '../component/Home';
+
+
+function Alert(props) {
+    return <MuiAlert severity="info" elevation={6} variant="filled" {...props} />;
+  }
 const Home=()=>{
+    const [open, setOpen] = React.useState(false);
+    var vertical= 'bottom';
+    var horizontal= 'left';
+    const handleClose = () => {
+        setOpen(false);
+      };
+      useEffect(()=>{
+          setOpen(true);
+          setTimeout(() => {
+              setOpen(false);
+          }, 5000);
+      },[])
     return (
         <>
+         <Snackbar
+            anchorOrigin={{ vertical, horizontal }}
+            open={open}
+            onClose={handleClose}
+            message="I love snacks"
+            key={vertical + horizontal}
+        >
+            <Alert onClose={handleClose} severity="info">
+                Di chuyển Top 100 để nghe nhạc :v
+            </Alert>
+        </Snackbar>
         <HomeComponent />
         </>
     )
